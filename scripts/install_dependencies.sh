8fd96212e55bb367e2e5a67d84fe40af0e7125de
amit00986 <aryan.amit0824@gmail.com>
2024-07-13 14:48:35 +0530

    chnages

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -6,12 +6,19 @@ PROJECT_DIR="/var/www/html"
 # Change directory to the project directory or exit if failed
 cd "$PROJECT_DIR" || exit 1
 
-# Clean node_modules and reinstall dependencies
-echo "Cleaning node_modules directory..."
+# Clean node_modules and package-lock.json
+echo "Cleaning node_modules and package-lock.json..."
 rm -rf node_modules package-lock.json
 
+# Install dependencies
 echo "Installing dependencies..."
 npm install
+
+# Check if npm install command was successful
+if [[ $? -ne 0 ]]; then
+  echo "Error: npm install failed!"
+  exit 1
+fi
 
 # Ensure react-scripts is installed correctly
 if ! npm list react-scripts >/dev/null 2>&1; then
@@ -30,4 +37,11 @@ echo "Building the React app..."
 # Run npm build script
 npm run build
 
-# Check if npm run
+# Check if npm run build command was successful
+if [[ $? -ne 0 ]]; then
+  echo "Error: npm run build failed!"
+  exit 1
+fi
+
+# Informative message upon successful build
+echo "Successfully built the React app!"
